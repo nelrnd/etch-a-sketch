fa43fb418d730a7aa0f678275c80b3f73866ed7a
Nel <nel.renaudin@gmail.com>
2022-08-30 13:46:16 +0200

    Implement change grid size slider

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const grid = document.getElementById('grid');
-const button = document.querySelector('button');
+const sizeSlider = document.getElementById('sizeSlider');
 
-function createGrid(size) {
+function createGrid(size = 16) {
+  // Show grid size in text format
+  document.querySelector('p#gridSize').textContent = `${size} x ${size}`;
+
   // Set nb of square per rows and columns accordingly to size
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -12,17 +15,18 @@ function createGrid(size) {
   // Create squares and add to grid
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement('div');
+    
     square.classList.add('square');
     square.addEventListener('mouseover', event => {
       event.target.classList.add('painted');
     })
+
     grid.appendChild(square);
   }
 }
 
-button.addEventListener('click', () => {
-  let size = prompt('Please type in the new grid dimensions (max is 100)');
-  if (size > 0 && size <= 100) createGrid(size);
-})
+sizeSlider.addEventListener('input', event => {
+  createGrid(event.target.value);
+});
 
-createGrid(16);
+window.addEventListener('load', () => createGrid());
